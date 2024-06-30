5709328711faea5828baa935a0b8b5665405cc73
Francisco Collazos <fcollazos@internetbrands.com>
2024-06-30 12:45:03 -0400

    add express, use template

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
-const server = Bun.serve({
-  port: 8080,
-  fetch(req) {
-    return new Response("Hello, World!");
-  },
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const port = 8080;
+app.use(express.json());
+
+app.post("/blog", (req: Request, res: Response) => {
+  //create new blog post
 });
 
-console.log(`Server is running on port ${server.port}`);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Api running");
+});
+
+app.get("/blog", (req: Request, res: Response) => {
+  //get all posts
+});
+
+app.get("/blog/:post", (req: Request, res: Response) => {
+  //get a specific post
+});
+
+app.delete("/blog/:post", (req: Request, res: Response) => {
+  //delete a post
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
