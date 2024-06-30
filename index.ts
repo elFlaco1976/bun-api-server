5ca9eef71b04cce656156a59764862c9247aa48d
Francisco Collazos <fcollazos@internetbrands.com>
2024-06-30 12:35:41 -0400

    basic http server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 8080,
+  fetch(req) {
+    return new Response("Hello, World!");
+  },
+});
+
+console.log(`Server is running on port ${server.port}`);
