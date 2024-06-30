50ee6b3d6e0771ef9bb62f1958a3a874d4539b72
Francisco Collazos <fcollazos@internetbrands.com>
2024-06-30 12:55:05 -0400

    add post model, run first db migration

---
+++ prisma/migrations/20240630165402_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "authorName" TEXT,
+    "authorEmail" TEXT,
+    "authorId" INTEGER
+);
